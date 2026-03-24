593794fd217d5f588fce0c5813d7e96d07773a13
kanghyekoung <kanghyekoung3694@gmail.com>
2026-03-24 09:09:55 +0900

    add

--- infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -7,9 +7,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class BackendController {
-
+	
     @GetMapping("/backend")
     public String backend() {
         return "backend";
+    }
+    
+    @GetMapping("/backend2")
+    public String backend2() {
+        return "backend2";
+    }
+    
+    @GetMapping("/backend3")
+    public String backend3() {
+        return "backend3";
+    }
+    
+    @GetMapping("/backend4")
+    public String backend4() {
+        return "backend4";
+    }
+    
+    @GetMapping("/backend5")
+    public String backend5() {
+        return "backend5";
     }
 }
